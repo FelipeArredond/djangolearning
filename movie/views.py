8a9af4a065a20a9ba37ea179adf7f864c7fc4e58
Felipe Arredond <pipechiqui77@gmail.com>
2022-08-02 09:53:37 -0500

    First commit

---
+++ movie/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    #return HttpResponse('<h1>Welcome to Home Page</h1>')
+    #return render(request,'home.html')
+    return render(request,'home.html', {'name':'Felipe Arredondo'})
